9d908ee596e6cd973a4054d8ba8d709fe839a2f1
T-rex <0x50Fc@gmail.com>
2022-08-23 18:50:10 +0800

    up

--- .github/workflows/ci/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-DIR=$(dirname $0)
-
-echo $DIR
-
-docker run --rm -v $DIR/../../..:/workdir -w "/workdir" docker.io/nginx/unit:1.27.0-go1.18 /workdir/.github/workflows/ci/docker_build.sh
