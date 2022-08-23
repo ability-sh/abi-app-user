f017c48eed626a58f524ef7cbb46bae8597e208a
T-rex <0x50Fc@gmail.com>
2022-08-23 18:53:01 +0800

    Update build.sh

--- .github/ci/build.sh
+++ .github/ci/build.sh
@@ -4,4 +4,4 @@ PWD_DIR=$(pwd)
 
 echo $PWD_DIR
 
-docker run --rm -v $PWD_DIR:/workdir -w "/workdir" docker.io/nginx/unit:1.27.0-go1.18 /workdir/.github/workflows/ci/docker_build.sh
+docker run --rm -v $PWD_DIR:/workdir -w "/workdir" docker.io/nginx/unit:1.27.0-go1.18 /workdir/.github/ci/docker_build.sh
